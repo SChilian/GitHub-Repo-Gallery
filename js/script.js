1ecb91512f3256f33e6ea4f68da927860dc50429
chilian_s <sierrachilian@gmail.com>
2024-01-23 21:38:26 -0500

    Fetch and added function to display repo information

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Where profile information will appear.//
 const profile = document.querySelector(".overview");
 const username = "SChilian";
+const repoList = document.querySelector(".repo-list");
 
 const gitInfo = async function () {
     const response = await fetch (`https://api.github.com/users/${username}`);
@@ -27,4 +28,25 @@ const userInfo = function(data) {
         </div>`;
 
         profile.append(div);
+        reposData();
 }
+
+const reposData = async function () {
+    const repoInfo = await fetch (`https://api.github.com/users/${username}/repos?sort=update&per_page=100`);
+
+    const results = await repoInfo.json();
+    console.log(results);
+    oneRepoInfo(results);
+    
+}
+
+const oneRepoInfo = function(repos){
+    for (const repo of repos) {
+        const li = document.createElement("li")
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+        reposData();
+    }
+}
+
